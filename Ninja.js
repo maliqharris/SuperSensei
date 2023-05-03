afde7ee1be86e1e7fa301d37b8ff74061d8026f2
maliqharris <127056176+maliqharris@users.noreply.github.com>
2023-05-03 10:52:53 -0400

    Add files via upload

---
+++ Ninja.js
@@ -0,0 +1,50 @@
+class Ninja{
+    constructor(name){
+        this.name = name;
+        this.health = 0;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName(){
+        console.log(this.name);
+    }
+    showStats(){
+        console.log(this.name, "-")
+        console.log("Strength:",this.strength);
+        console.log("Speed:",this.speed );
+        console.log("Health:", this.health);
+    }
+    drinkSake(){
+        this.health += 10;
+    }
+}
+class Sensei extends Ninja{
+    constructor(name){
+        super(name)
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        const message = super.drinkSake();
+        message;
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
+
+
+
+
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
